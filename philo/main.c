1baa6aa69556e5d2e13d20e72379581926ae6595
Erick Gonçalves <erickbarros06@hotmail.com>
2023-01-21 17:15:08 +0000

    eat action

--- philo/main.c
+++ philo/main.c
@@ -6,24 +6,42 @@
 /*   By: egoncalv <egoncalv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:33:40 by egoncalv          #+#    #+#             */
-/*   Updated: 2023/01/13 18:38:25 by egoncalv         ###   ########.fr       */
+/*   Updated: 2023/01/21 17:14:02 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#include <unistd.h>
 
-void	*thread_function(void *i)
+void	*choose_action(void *philosopher)
 {
-	int	*idx;
+	t_phi	*phi;
 
-	idx = (int *)i;
-	idx++;
-	i = idx;
-	return (i);
+	phi = (t_phi *)philosopher;
+	if (phi->id % 2)
+		usleep(10000);
+	eat_action(philosopher);
+	printf("%d\t", get_time_ms());
+	printf("%d is sleeping\n", phi->id);
+	usleep(1000000);
+	return (philosopher);
 }
 
-t_fork	*create_fork()
+void	eat_action(t_phi *phi)
+{
+	pthread_mutex_lock(&phi->r_fork->mutex);
+	printf("%d\t", get_time_ms());
+	printf("%d has taken a fork\n", phi->id);
+	pthread_mutex_lock(&phi->l_fork->mutex);
+	printf("%d\t", get_time_ms());
+	printf("%d has taken a fork\n", phi->id);
+	printf("%d\t", get_time_ms());
+	printf("%d is eating\n", phi->id);
+	usleep(phi->info->t_eat * 1000);
+	pthread_mutex_unlock(&phi->r_fork->mutex);
+	pthread_mutex_unlock(&phi->l_fork->mutex);
+}
+
+t_fork	*create_fork(void)
 {
 	t_fork				*fork;
 	static int			i;
@@ -36,7 +54,8 @@ t_fork	*create_fork()
 	return (fork);
 }
 
-t_phi	**create_phi(int p_num)
+//Remember to specify if there is only one philosopher, create only one fork
+t_phi	**create_phi(t_info *info)
 {
 	t_phi		**philosophers;
 	t_fork		*right_fork;
@@ -44,12 +63,13 @@ t_phi	**create_phi(int p_num)
 	int			i;
 
 	i = 0;
-	philosophers = malloc(sizeof(t_phi *) * p_num + 1);
+	philosophers = malloc(sizeof(t_phi) * info->p_num + 1);
 	left_fork = 0;
-	while (i < p_num)
+	while (i < info->p_num)
 	{
 		philosophers[i] = malloc(sizeof(t_phi));
-		pthread_create(&philosophers[i]->tid, NULL, thread_function, &i);
+		philosophers[i]->id = i;
+		philosophers[i]->info = info;
 		right_fork = create_fork();
 		philosophers[i]->r_fork = right_fork;
 		philosophers[i]->l_fork = left_fork;
@@ -57,6 +77,9 @@ t_phi	**create_phi(int p_num)
 		i++;
 	}
 	philosophers[0]->l_fork = right_fork;
+	i = -1;
+	while (++i < info->p_num)
+		pthread_create(&philosophers[i]->tid, NULL, choose_action, philosophers[i]);
 	return (philosophers);
 }
 
@@ -69,13 +92,9 @@ int	main(int argc, char **argv)
 	if (check_args(argc, argv))
 	{
 		set_info(&info, argv);
-		philosophers = create_phi(info.p_num);
+		philosophers = create_phi(&info);
 	}
-	for(int i = 0; i < info.p_num; i++)
-	{
-		printf("Philosopher %d ", i);
-		printf("has left fork %d", philosophers[i]->l_fork->id);
-		printf("and right fork %d\n", philosophers[i]->r_fork->id);
-	}
+	for (int i = 0; i < info.p_num; i++)
+		pthread_join(philosophers[i]->tid, NULL);
 	return (1);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: egoncalv <egoncalv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 15:22:51 by egoncalv          #+#    #+#             */
-/*   Updated: 2023/01/13 18:24:35 by egoncalv         ###   ########.fr       */
+/*   Updated: 2023/01/21 17:14:13 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdint.h>
+# include <unistd.h>
 
 typedef struct s_info
 {
@@ -40,6 +41,7 @@ typedef struct s_phi
 {
 	t_info		*info;
 	pthread_t	tid;
+	int			id;
 	t_fork		*l_fork;
 	t_fork		*r_fork;
 }				t_phi;
@@ -49,5 +51,6 @@ int		ft_atoi(const char *str);
 int		is_nbr(char	*argv);
 int		check_args(int argc, char **argv);
 void	set_info(t_info *info, char	**argv);
+void	eat_action(t_phi *phi);
 
 #endif
